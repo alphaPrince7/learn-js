82d9dd9b22a12efecd47ea1efd3acd29d59cd092
Prince Sharma <alphaprince0093@gmail.com>
2026-08-11 20:14:48 +0000

    basics of programming language

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 978455
+let accountEmail = "prince@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 not allowed
+
+accountEmail = "alphaprince@gamil.com"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+// Let has block scope
+// Var has functional scope
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 3); 
+
+console.log("Prince Sharma");
+
+let name = "Prince Sharma"
+let age = 24
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+
+// object 
+
+console.log(typeof "Prince")
+
+console.log(typeof null)
+console.log(typeof undefined)
