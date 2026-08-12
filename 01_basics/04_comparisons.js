c9dc2227b1f8718e763a4b985f127838a0165bf7
Prince Sharma <alphaprince0093@gmail.com>
2026-08-12 10:24:12 +0000

    comparison operators

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+
+// console.log("2" > 1);
+// console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0) // comparison convert null to a number, treating it as 0.
+// That's why (3) null >=0 is true and (1) null > 0 is false
+
+console.log(undefined >= 0)
+
+// ===
+
+console.log("2" === 2)
