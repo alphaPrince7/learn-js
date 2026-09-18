0cdd21838759a28f36d4a2074dc9944658098824
Prince Sharma <alphaprince0093@gmail.com>
2026-09-18 17:24:46 +0000

    basics of iterations (while and do while loops)

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+
+let i =0
+while(i<= 10){
+    console.log(`value of index is ${i}`)
+    i = i+2
+}
+
+let myArr = ["flash", "batman", "superman"]
+
+let arr = 0
+while(arr < myArr.length){
+    console.log(`value is ${myArr[arr]}`)
+    arr = arr + 1
+}
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10)
